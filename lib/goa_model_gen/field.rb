5a3435ae4825af4588e7cc052dc011158a878f7f
akm <akm2000@gmail.com>
2018-07-10 19:46:55 +0900

    :+1: Add golang_type method and format field

--- lib/goa_model_gen/field.rb
+++ lib/goa_model_gen/field.rb
@@ -6,6 +6,7 @@ require "active_support/core_ext/string"
 module GoaModelGen
   class Field
     attr_reader :name, :type, :default
+    attr_accessor :format # for swagger. See https://swagger.io/docs/specification/data-models/data-types/
     attr_accessor :required
     attr_reader :type_obj
     attr_reader :datastore_tag
@@ -13,6 +14,7 @@ module GoaModelGen
     def initialize(name, attrs)
       @name = name
       @type = attrs['type']
+      @format = attrs['format']
       @required = attrs['required']
       @default = attrs['default']
       @datastore_tag = attrs['datastore_tag']
@@ -57,6 +59,29 @@ module GoaModelGen
         ['validate', validate_tag],
         ['datastore', datastore_tag],
       ].map{|k,v| v ? "#{k}:\"#{v}\"" : nil}.compact.join(' ')
+    end
+
+    # https://swagger.io/docs/specification/data-models/data-types/
+    # https://tour.golang.org/basics/11
+    # https://golang.org/pkg/go/types/#pkg-variables
+    SWAGGER_TYPE_TO_GOLANG_TYPE = {
+      "string" => Hash.new("string").update(
+        "date" => "time.Time",
+        "date-time" => "time.Time",
+      ),
+      "number" => Hash.new("float32").update(
+        "double" => "float64",
+      ),
+      "integer" => Hash.new("int").update(
+        "int64" => "int64",
+      ),
+      "boolean" => Hash.new("bool"),
+    }
+
+    def golang_type
+      format2type = SWAGGER_TYPE_TO_GOLANG_TYPE[type]
+      raise "Golang type not found for #{type}" unless format2type
+      return format2type[format]
     end
   end
 end
