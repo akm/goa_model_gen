df120672716301553f25e281c844fc5e4bfc68e1
akm <akm2000@gmail.com>
2018-08-20 17:21:23 +0900

    :+1: Use config instead of options

--- lib/goa_model_gen/cli.rb
+++ lib/goa_model_gen/cli.rb
@@ -2,19 +2,19 @@ require "goa_model_gen"
 
 require "thor"
 
+require "goa_model_gen/config"
 require "goa_model_gen/loader"
 require "goa_model_gen/generator"
 
 module GoaModelGen
   class Cli < Thor
+    class_option :config, type: :string, aliases: 'c', default: './goa_model_gen.yaml', desc: 'Path to config file. You can generate it by config subcommand'
+
     desc "config", "Generate config file"
-    option :path, type: :string, default: './goa_model_gen.yaml', desc: 'Path to config file'
-    def config
+    def config(path = './goa_model_gen.yaml')
       open(path, 'w'){|f| f.puts(Config.new.fulfill.to_yaml) }
     end
 
-    class_option :go_package, type: :string, default: default_go_package, desc: 'Base go package name'
-    class_option :swagger_yaml, type: :string, default: './swagger/swagger.yaml', desc: 'Swagger definition YAML file'
 
     desc "show FILE1...", "Show model info from definition files"
     def show(*paths)
@@ -25,47 +25,38 @@ module GoaModelGen
     end
 
     desc "model FILE1...", "Generate model files from definition files"
-    option :dir, type: :string, default: './model', desc: 'Output directory path'
-    option :gofmt, type: :boolean, default: true, desc: 'Run gofmt for generated file'
     def model(*paths)
       generator = new_generator
       load_types_for(paths) do |path, types|
-        dest = File.join(options[:dir], File.basename(path, ".*") + ".go")
+        dest = File.join(cfg.model_dir, File.basename(path, ".*") + ".go")
         generator.run('templates/model.go.erb', types, dest)
-        if options[:gofmt]
-          system("gofmt -w #{dest}")
-        end
+        system("gofmt -w #{dest}") unless cfg.gofmt_disabled
       end
     end
 
     desc "converter FILE1...", "Generate converter files from definition files and swagger.yaml"
-    option :package, type: :string, default: 'controller', desc: 'package name'
-    option :dir, type: :string, default: './controller', desc: 'Output directory path'
-    option :gofmt, type: :boolean, default: true, desc: 'Run gofmt for generated file'
     def converter(*paths)
       generator = new_generator
       load_types_for(paths) do |path, types|
-        dest = File.join(options[:dir], File.basename(path, ".*") + "_conv.go")
+        dest = File.join(cfg.controller_dir, File.basename(path, ".*") + "_conv.go")
         if types.any?{|t| !!t.payload || !!t.media_type}
           generator.run('templates/converter.go.erb', types, dest)
-          if options[:gofmt]
-            system("gofmt -w #{dest}")
-          end
+          system("gofmt -w #{dest}") unless cfg.gofmt_disabled
         end
       end
     end
 
     no_commands do
-      def config
-        @config ||= GoaModelGen::Config.new.load_from(options[:config)
+      def cfg
+        @cfg ||= GoaModelGen::Config.new.load_from(options[:config])
       end
 
       def new_generator
-        GoaModelGen::Generator.new(config)
+        GoaModelGen::Generator.new(cfg)
       end
 
       def load_types_for(paths)
-        swagger_loader = GoaModelGen::SwaggerLoader.new(config.swagger_yaml)
+        swagger_loader = GoaModelGen::SwaggerLoader.new(cfg.swagger_yaml)
         path_to_types = {}
         defined_types = {}
         paths.each do |path|
