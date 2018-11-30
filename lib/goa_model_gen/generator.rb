912a3c5080df1ab8f1e4862b230f79a8c3a3c3f9
akm <akm2000@gmail.com>
2018-11-30 21:16:29 +0900

    :+1: Define import method to tell dependencies in template

--- lib/goa_model_gen/generator.rb
+++ lib/goa_model_gen/generator.rb
@@ -20,11 +20,43 @@ module GoaModelGen
       @golang_helper ||= GolangHelper.new
     end
 
+    def package(name = nil)
+      if name
+        @package = name
+      else
+        @package
+      end
+    end
+
+    def dependencies
+      @dependencies ||= []
+    end
+
+    def clear_dependencies
+      @dependencies = nil
+    end
+
+    def import(*packages)
+      packages.each do |package|
+        dependencies.push(package) unless dependencies.include?(package)
+      end
+    end
+
+    GO_BASE_PATH = File.expand_path('../templates/base.go.erb', __FILE__)
+
     def generate(template_path)
+      clear_dependencies
+
       abs_path = File.expand_path('../' + template_path, __FILE__)
       erb = ERB.new(File.read(abs_path), nil, "-")
       erb.filename = abs_path
-      content = erb.result(binding)
+      body = erb.result(binding).strip
+
+      raise "No package given in #{abs_path}" if package.blank?
+
+      base = ERB.new(File.read(GO_BASE_PATH), nil, "-")
+      base.filename = GO_BASE_PATH
+      base.result(binding).strip
     end
 
     def run(template_path, output_path, overwrite: false)
